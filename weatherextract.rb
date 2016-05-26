d9994891d3e4c6869a3ceb3444fda5f2322ae7d9
Carson Wood <carsnwd@gmail.com>
2016-05-26 16:07:49 -0400

    Delete weatherextract.rb

--- weatherextract.rb
+++
@@ -1,8 +0,0 @@
-require 'forecast_io'
-
-ForecastIO.configure do |configuration|
-  configuration.api_key = '16e00305725189eb6eff8d1c87cbc886'
-end
-
-forecast = ForecastIO.forecast(40.0506, -77.5203)
-puts forecast.currently.icon
